74e905a3046f87eb6ceff08e5094b9bd1c37b7dc
claytongentry <claytongentry2017@u.northwestern.edu>
2021-06-08 10:58:16 -0500

    Include body in unit test

--- lib/gam.test.ts
+++ lib/gam.test.ts
@@ -2,11 +2,19 @@ import nock from "nock"
 import { assert } from "chai"
 import { createTaglessRequest } from "./gam"
 
+const creative = `
+  <script>
+    var payload = {
+      hello: '[%hello%]'
+    }
+  </script>
+`
+
 function mockSuccess(parameters: Record<string, any>) {
   nock("https://securepubads.g.doubleclick.net")
     .get("/gampad/adx")
     .query(parameters)
-    .reply(200)
+    .reply(200, creative)
 }
 
 describe("createTaglessRequest", () => {
@@ -21,7 +29,8 @@ describe("createTaglessRequest", () => {
 
       mockSuccess(parameters)
       const response = await createTaglessRequest(parameters)
-      assert.isTrue(response.ok)
+      assert.equal(response.status, 200)
+      assert.isOk(await response.text())
     })
   })
 })
