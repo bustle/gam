3a75c9504bd1a355e96dc3d351fb431d703124b4
claytongentry <claytongentry2017@u.northwestern.edu>
2021-06-13 17:44:26 -0500

    Fix gitignore

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+import TaglessRequestParameters from "./types/taglessRequestParameters"
+
+export { TaglessRequestParameters }
+
+export function createTaglessRequest(
+  parameters: TaglessRequestParameters
+): Response
