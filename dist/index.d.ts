f97ef46a56f4e909aab3789a86902860ff5485be
claytongentry <claytongentry2017@u.northwestern.edu>
2021-06-13 17:46:27 -0500

    relocate

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import TaglessRequestParameters from "./types/taglessRequestParameters"
-
-export { TaglessRequestParameters }
-
-export function createTaglessRequest(
-  parameters: TaglessRequestParameters
-): Response
